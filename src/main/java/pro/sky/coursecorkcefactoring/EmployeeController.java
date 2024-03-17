98fe7d96c6cbd7ae0c8a404f938a2d3c7e6ce741
SaintEduis <edik.melk@yandex.ru>
2024-03-18 00:38:12 +0300

    Готовое ДЗ!

--- src/main/java/pro/sky/coursecorkcefactoring/EmployeeController.java
+++ src/main/java/pro/sky/coursecorkcefactoring/EmployeeController.java
@@ -1,15 +1,14 @@
 package pro.sky.coursecorkcefactoring;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pro.sky.coursecorkcefactoring.expections.EmployeeAlreadyAddedException;
 import pro.sky.coursecorkcefactoring.expections.EmployeeStorageIsFullException;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController{
     private final EmployeeService employeeService;
 
@@ -17,22 +16,22 @@ public class EmployeeController{
         this.employeeService = employeeService;
     }
 
-    @GetMapping(path = "/employee/add")
+    @GetMapping(path = "/add")
     public Employee addEmployee(@RequestParam("firstName") String fName, @RequestParam("lastName") String lName) {
         return employeeService.addEmployee(fName, lName);
     }
 
-    @GetMapping(path = "/employee/remove")
+    @GetMapping(path = "/remove")
     public Employee deketeEmployee(@RequestParam("firstName") String fName, @RequestParam("lastName") String lName) {
         return employeeService.deleteEmployee(fName, lName);
     }
 
-    @GetMapping(path = "/employee/find")
+    @GetMapping(path = "/find")
     public Employee findEmployee(@RequestParam("firstName") String fName, @RequestParam("lastName") String lName) {
         return employeeService.findEmployee(fName, lName);
     }
 
-    @GetMapping(path = "/employee/getAll")
+    @GetMapping(path = "/getAll")
     public List<Employee> getAllEmployee() {
         return employeeService.getAllEmployee();
     }
