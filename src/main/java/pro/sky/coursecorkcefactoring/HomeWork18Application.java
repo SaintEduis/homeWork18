859391da3e66f3a87a5d5ffce76c3effbe18f8f1
SaintEduis <edik.melk@yandex.ru>
2024-03-17 03:33:15 +0300

    First commit!

---
+++ src/main/java/pro/sky/coursecorkcefactoring/HomeWork18Application.java
@@ -0,0 +1,13 @@
+package pro.sky.coursecorkcefactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork18Application.class, args);
+	}
+
+}
